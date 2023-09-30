7e2008429315bd57cf778529a5dd7ff919566046
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2023-09-30 17:53:51 -0600

    published

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,16 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+let cart = new Array();
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cartContent = getLocalStorage("so-cart");
+    if (cartContent) {
+    cart = cartContent;
+  }
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {

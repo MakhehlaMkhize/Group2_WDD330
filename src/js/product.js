210d3c016c13d9a2900ade8b8a7d915837d9cfad
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2023-10-02 12:49:57 -0600

    dynamic product details

--- src/js/product.js
+++ src/js/product.js
@@ -1,25 +1,9 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import getParams from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
+const productId = getParams('product');
 const dataSource = new ProductData("tents");
-function addProductToCart(product) {
-  let cartContent = getLocalStorage("so-cart");
 
-  // if cart is empty, create an empty array
-  if (cartContent === null) {
-    cartContent = new Array();
-  }
-
-  cartContent.push(product);
-  setLocalStorage("so-cart", cartContent);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
